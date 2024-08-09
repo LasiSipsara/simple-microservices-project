f69469c8ad0f5b9bd8111865afd8ded15b16caa8
LasiSipsara <sipsaralasini@gmail.com>
2024-08-09 15:39:16 +0530

    uploaded service registry

---
+++ service-registry/src/test/java/com/lasinisipsara/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lasinisipsara.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
